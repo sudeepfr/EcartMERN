ac707da77d2d66b5cf9b965476186cebbeb5fd98
sudeepfr <sudeepverma111213@gmail.com>
2026-01-14 01:48:11 +0530

    added store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cart from "../features/cart/cartSlice.js";
+import product from "../features/products/productSlice.js"
+import filter from "../features/filter/filterSlice.js"
 
 const store=configureStore({
   reducer:{
     product,
+    filter,
+    cart
   }
 
 })
